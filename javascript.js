5a464545e01f8859d6172929d414ab1c681012a6
Laxman879 <laxman.a@brewcode.co>
2024-05-13 13:33:27 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,21 @@
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+
+function changeBgToGreyAndUpdateText() {
+  colorPickerContainer.style.backgroundColor = "#e0e0e0";
+  selectedColorHexCode.textContent = "#e0e0e0";
+}
+
+function changeBgToGreenAndUpdateText() {
+  colorPickerContainer.style.backgroundColor = "#6fcf97";
+  selectedColorHexCode.textContent = "##6fcf97";
+}
+
+function changeBgToBlueAndUpdateText() {
+  colorPickerContainer.style.backgroundColor = "#56ccf2";
+  selectedColorHexCode.textContent = "#56ccf2";
+}
+function changeBgToParpulAndUpdateText() {
+  colorPickerContainer.style.backgroundColor = "#bb6bd9";
+  selectedColorHexCode.textContent = "#bb6bd9";
+}
